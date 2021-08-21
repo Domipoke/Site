a65163ed5d942beb407f8f5862672b2d89c13065
Domipoke <42298508+Domipoke@users.noreply.github.com>
2021-08-22 01:28:43 +0200

    upd

--- script/BetterHtml.js
+++ script/BetterHtml.js
@@ -3,8 +3,21 @@ class tooltip extends HTMLElement {
         super()
     }
 }
+class tooltiptext extends HTMLElement {
+    constructor() {
+        super()
+    }
+}
+class tooltipcontent extends HTMLElement {
+    constructor() {
+        super()
+    }
+}
+
 
 
 //Add element
 customElements.define("tool-tip", tooltip) 
+customElements.define("tool-tip-text", tooltip) 
+customElements.define("tool-tip-content", tooltip) 
 //
