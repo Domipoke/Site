48a686d2643e3c737c3f181af8f5d9652c966227
Domipoke <domenico.montuori1312006@gmail.com>
2021-08-26 00:41:03 +0200

    update

--- script/BetterHtml.js
+++ script/BetterHtml.js
@@ -1,3 +1,5 @@
+/* TOOL TIP */
+
 class tooltip extends HTMLDivElement {
     constructor() {
         super()
@@ -16,8 +18,26 @@ class tooltipcontent extends HTMLElement {
 
 
 
-//Add element
 customElements.define("tool-tip", tooltip) 
 customElements.define("tool-tip-text", tooltiptext) 
 customElements.define("tool-tip-content", tooltipcontent) 
-////
+
+/* DIV MENU */
+/* 
+"vertical"|"horizontal"|"fixedtop"|"fixedbottom"|"stickytop"|"stickybottom" 
+*/
+class divmenu extends HTMLDivElement {
+    constructor () {
+        super()
+        
+    }
+}
+
+class menuitem extends HTMLDivElement {
+    constructor () {
+        super()
+    }
+}
+
+customElements.define("div-menu", divmenu)
+customElements.define("menu-item", menuitem)
