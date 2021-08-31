9eb900155c8019a9b892377f133be361fa01e479
Domipoke <42298508+Domipoke@users.noreply.github.com>
2021-08-31 23:19:55 +0200

    Delete script/LogIn directory

--- script/LogIn/Form.js
+++
@@ -1,4 +0,0 @@
-function LoginOpen() {
-    var form = '<log-in type="email: true; password: true;"></log-in>'
-    if (!document.body.innerHTML.includes(form)) {document.body.innerHTML = form + document.body.innerHTML}
-}
